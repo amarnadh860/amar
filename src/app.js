1957403fcfd45bbf970a14494d2df5d99fd73feb
Amarnadh Kondragunta <amarnadh6904@gmail.com>
2022-04-18 13:15:51 +0530

    welcome and copyright components added and modified styles

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Bio from "./components/Bio";
-import Projects from "./components/Projects"
-import MyContact from "./components/Contact";
+import Copyright from "./components/Copyright";
+import Projects from "./components/Projects";
+import Welcome from "./components/Welcome";
 const app = document.getElementById('app')
 const jsx = (
     <div id='portfolio'>
        <fieldset>
+       <Welcome/>
        <Bio/>
        <Projects/> 
+       <Copyright/>
        </fieldset>
     </div>  
 )
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -14,22 +14,17 @@ class Bio extends React.Component{
     <div id = 'bio'>
         <div id="profile-card">
             <div>
+                <p>Contact card</p>
                 <img src ={profile} alt = 'profile picture' id = 'profilepic' />
                 <h1>Amarnadh</h1>
-                <h2>Web developer</h2>
+                <p>Web developer</p>
                 <Contact />
             </div>
-            {/*<div>
-                <h2>Want to Contact me!</h2>
-                <h3><a href = " https://www.linkedin.com/in/amarnadh-kondragunta" target="_blank">Linkedin</a></h3>
-                <h3><a href = " mailto:amarnadh6904@gmail.com" target="_blank">Gmail</a></h3>
-                <h3><a href = " https://github.com/amarnadh860" target="_blank">Github</a></h3>
-    </div>*/}
-                
-
+    
         </div>
 
         <div id = "profile-details">
+            <p id = "heading">Introduction</p>
             <p>Hi there, This is <strong>Amarnadh Kondragunta</strong></p>
             <p>I am a passionate software engineer who loves to code in <strong>javascript and react</strong> for front end</p>
             <p>Loves to code in <strong>Java and python</strong> for backend</p>
